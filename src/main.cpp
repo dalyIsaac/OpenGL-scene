5f354cb6fb7e76c702782c831ef7dc34bdc6d6d5
dalyIsaac <isaac.daly@outlook.com>
2019-04-05 16:46:20 +1300

    Added castle

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,17 @@
-#include <iostream>
-#include <fstream>
+#include <GL/freeglut.h>
 #include <climits>
 #include <cmath>
-#include <GL/freeglut.h>
+#include <fstream>
+#include <iostream>
 using namespace std;
 
-float *x;
-float *y;
-float *z;
+float* x;
+float* y;
+float* z;
 
-int *t1;
-int *t2;
-int *t3;
+int* t1;
+int* t2;
+int* t3;
 
 int num_vertices;
 int num_triangles;
@@ -33,25 +33,25 @@ float wall_thickness = 6.0;
 
 /**
  * @brief Loads the OFF mesh file.
- * 
- * @param fname 
+ *
+ * @param fname
  */
-void loadMeshFile(const char *fname)
+void loadMeshFile(const char* fname)
 {
     ifstream fp_in;
     int num, ne;
 
     fp_in.open(fname, ios::in);
-    if (!fp_in.is_open())
-    {
+    if (!fp_in.is_open()) {
         cout << "Error opening mesh file" << endl;
         exit(1);
     }
 
-    fp_in.ignore(INT_MAX, '\n');                  //ignore first line
-    fp_in >> num_vertices >> num_triangles >> ne; // read number of vertices, polygons, edges
+    fp_in.ignore(INT_MAX, '\n'); // ignore first line
+    fp_in >> num_vertices >> num_triangles >>
+        ne; // read number of vertices, polygons, edges
 
-    x = new float[num_vertices]; //create arrays
+    x = new float[num_vertices]; // create arrays
     y = new float[num_vertices];
     z = new float[num_vertices];
 
@@ -59,15 +59,15 @@ void loadMeshFile(const char *fname)
     t2 = new int[num_triangles];
     t3 = new int[num_triangles];
 
-    for (int i = 0; i < num_vertices; i++) //read vertex list
+    for (int i = 0; i < num_vertices; i++) // read vertex list
         fp_in >> x[i] >> y[i] >> z[i];
 
-    for (int i = 0; i < num_triangles; i++) //read polygon list
+    for (int i = 0; i < num_triangles; i++) // read polygon list
     {
         fp_in >> num >> t1[i] >> t2[i] >> t3[i];
-        if (num != 3)
-        {
-            cout << "ERROR: Polygon with index " << i << " is not a triangle." << endl; //not a triangle!!
+        if (num != 3) {
+            cout << "ERROR: Polygon with index " << i << " is not a triangle."
+                 << endl; // not a triangle!!
             exit(1);
         }
     }
@@ -78,8 +78,8 @@ void loadMeshFile(const char *fname)
 
 /**
  * @brief Calculates the normal vector of a triangle.
- * 
- * @param tindx 
+ *
+ * @param tindx
  */
 void normal(int tindx)
 {
@@ -102,37 +102,36 @@ void myTimer(int value)
 
 /**
  * @brief Handles keyboard callbacks.
- * 
- * @param key 
- * @param x 
- * @param y 
+ *
+ * @param key
+ * @param x
+ * @param y
  */
 void special(int key, int x, int y)
 {
-    switch (key)
-    {
-    case GLUT_KEY_UP:
-        eye_x += 0.5 * sin(camera_angle);
-        eye_z -= 0.5 * cos(camera_angle);
-        break;
-    case GLUT_KEY_DOWN:
-        eye_x -= 0.5 * sin(camera_angle);
-        eye_z += 0.5 * cos(camera_angle);
-        break;
-    case GLUT_KEY_LEFT:
-        camera_angle -= radians_five;
-        break;
-    case GLUT_KEY_RIGHT:
-        camera_angle += radians_five;
-        break;
-    case GLUT_KEY_PAGE_UP:
-        eye_y++;
-        break;
-    case GLUT_KEY_PAGE_DOWN:
-        eye_y--;
-        break;
-    default:
-        break;
+    switch (key) {
+        case GLUT_KEY_UP:
+            eye_x += 0.5 * sin(camera_angle);
+            eye_z -= 0.5 * cos(camera_angle);
+            break;
+        case GLUT_KEY_DOWN:
+            eye_x -= 0.5 * sin(camera_angle);
+            eye_z += 0.5 * cos(camera_angle);
+            break;
+        case GLUT_KEY_LEFT:
+            camera_angle -= radians_five;
+            break;
+        case GLUT_KEY_RIGHT:
+            camera_angle += radians_five;
+            break;
+        case GLUT_KEY_PAGE_UP:
+            eye_y++;
+            break;
+        case GLUT_KEY_PAGE_DOWN:
+            eye_y--;
+            break;
+        default:
+            break;
     }
 
     look_x = eye_x + 100 * sin(camera_angle);
@@ -143,15 +142,14 @@ void special(int key, int x, int y)
 
 /**
  * @brief Draws a grid of lines on the floor plane.
- * 
+ *
  */
 void drawFloor()
 {
-    glColor3f(0., 0.5, 0.); //Floor colour
+    glColor3f(0., 0.5, 0.); // Floor colour
 
-    for (int i = -100; i <= 100; i++)
-    {
-        glBegin(GL_LINES); //A set of grid lines on the xz-plane
+    for (int i = -100; i <= 100; i++) {
+        glBegin(GL_LINES); // A set of grid lines on the xz-plane
         glVertex3f(-100, -0.75, i);
         glVertex3f(100, -0.75, i);
         glVertex3f(i, -0.75, -100);
@@ -162,15 +160,14 @@ void drawFloor()
 
 /**
  * @brief Draws the cannon.
- * 
+ *
  */
 void drawCannon()
 {
     glColor3f(0.4, 0.5, 0.4);
 
     glBegin(GL_TRIANGLES);
-    for (int tindx = 0; tindx < num_triangles; tindx++)
-    {
+    for (int tindx = 0; tindx < num_triangles; tindx++) {
         normal(tindx);
         glVertex3d(x[t1[tindx]], y[t1[tindx]], z[t1[tindx]]);
         glVertex3d(x[t2[tindx]], y[t2[tindx]], z[t2[tindx]]);
@@ -181,7 +178,7 @@ void drawCannon()
 
 /**
  * @brief Draws the castle, and it's constituent supports.
- * 
+ *
  */
 void cannon()
 {
@@ -227,51 +224,47 @@ void cannon()
     glPopMatrix();
 }
 
-/**
- * @brief Draws the castle.
- * 
- */
-void castle()
+void castleTower(GLfloat x = 0.0, GLfloat y = 0.0, GLfloat z = 0.0)
 {
-    // Left column
     glPushMatrix();
-    glTranslatef(20.0, 0.0, 0.0);
+    glTranslatef(x, y, z);
+
+    // Tower base
+    glPushMatrix();
     glRotatef(-90.0, 1.0, 0.0, 0.0);
     glutSolidCylinder(5.0, tower_height, 100.0, 100.0);
     glPopMatrix();
 
-    // Left tower roof
+    // Tower root
     glPushMatrix();
-    glTranslatef(20.0, tower_height, 0.0);
+    glTranslatef(0.0, tower_height, 0.0);
     glRotatef(-90.0, 1.0, 0.0, 0.0);
     glutSolidCone(7.0, 10.0, 100.0, 100.0);
     glPopMatrix();
 
-    // Right column
-    glPushMatrix();
-    glTranslatef(-20.0, 0.0, 0.0);
-    glRotatef(-90.0, 1.0, 0.0, 0.0);
-    glutSolidCylinder(5.0, tower_height, 100.0, 100.0);
     glPopMatrix();
+}
 
-    // Right tower roof
+void castleWall(GLfloat translate_x = 0.0, GLfloat translate_y = 0.0,
+                GLfloat translate_z = 0.0, GLfloat angle = 0.0,
+                GLfloat rotate_x = 0.0, GLfloat rotate_y = 0.0,
+                GLfloat rotate_z = 0.0)
+{
     glPushMatrix();
-    glTranslatef(-20.0, tower_height, 0.0);
-    glRotatef(-90.0, 1.0, 0.0, 0.0);
-    glutSolidCone(7.0, 10.0, 100.0, 100.0);
-    glPopMatrix();
+    glTranslatef(translate_x, translate_y, translate_z);
+    glRotatef(angle, rotate_x, rotate_y, rotate_z);
 
     // Left front wall
     glPushMatrix();
-    glTranslatef(-10.0, wall_height / 2, 0.0);
-    glScalef(10.0, wall_height, wall_thickness);
+    glTranslatef(-12.5, wall_height / 2, 0.0);
+    glScalef(15.0, wall_height, wall_thickness);
     glutSolidCube(1.0);
     glPopMatrix();
 
     // Right front wall
     glPushMatrix();
-    glTranslatef(10.0, wall_height / 2, 0.0);
-    glScalef(10.0, wall_height, wall_thickness);
+    glTranslatef(12.5, wall_height / 2, 0.0);
+    glScalef(15.0, wall_height, wall_thickness);
     glutSolidCube(1.0);
     glPopMatrix();
 
@@ -281,11 +274,40 @@ void castle()
     glScalef(10.0, wall_height / 2, wall_thickness);
     glutSolidCube(1.0);
     glPopMatrix();
+
+    glPopMatrix();
+}
+
+/**
+ * @brief Draws the castle.
+ *
+ */
+void castle()
+{
+    // Right front tower
+    castleTower(20.0);
+    // Front
+    castleWall();
+    // Left front tower
+    castleTower(-20.0);
+
+    // Right back tower
+    castleTower(20.0, 0.0, -40.0);
+    // Back
+    castleWall(0.0, 0.0, -40.0);
+    // Left back tower
+    castleTower(-20.0, 0.0, -40.0);
+
+    // Right side wall
+    castleWall(20.0, 0.0, -20.0, 90.0, 0.0, 1.0, 0.0);
+
+    // Left side wall
+    castleWall(-20.0, 0.0, -20.0, 90.0, 0.0, 1.0, 0.0);
 }
 
 /**
  * @brief Contains function calls for generating the scene.
- * 
+ *
  */
 void display(void)
 {
@@ -316,7 +338,7 @@ void display(void)
 
 /**
  * @brief Initializes OpenGL states.
- * 
+ *
  */
 void initialize(void)
 {
@@ -340,12 +362,12 @@ void initialize(void)
 
 /**
  * @brief Initializes the glut window, and registers callbacks.
- * 
- * @param argc 
- * @param argv 
- * @return int 
+ *
+ * @param argc
+ * @param argv
+ * @return int
  */
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_DEPTH);
