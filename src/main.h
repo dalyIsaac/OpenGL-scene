b6e65082efa47bccbb39b7997c62c276becbc420
dalyIsaac <isaac.daly@outlook.com>
2019-04-10 10:01:38 +1200

    Added a new nose cone surface shape

--- src/main.h
+++ src/main.h
@@ -13,3 +13,5 @@ extern float light_pos[4];
 extern float shadowMat[16];
 
 void normal(int tindx);
+void normal(float x1, float y1, float z1, float x2, float y2, float z2,
+            float x3, float y3, float z3);
