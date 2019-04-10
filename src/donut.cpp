e08fca797c6ecce190317cb256a007b5da18f2d6
dalyIsaac <isaac.daly@outlook.com>
2019-04-11 09:49:01 +1200

    Documentation and review for donut.cpp

--- src/donut.cpp
+++ src/donut.cpp
@@ -2,14 +2,41 @@
 #include "sweepSurfaces.h"
 #include <GL/freeglut.h>
 
+/**
+ * @brief The number of vertices inside the donut's base sweep surface.
+ *
+ */
 const int N = 2;
+
+/**
+ * @brief The components for the vertices in the x-axis for the donut's base
+ * sweep surface.
+ *
+ */
 static float x_points[N] = {5, 5};
+
+/**
+ * @brief The components for the vertices in the y-axis for the donut's base
+ * sweep surface.
+ *
+ */
 static float y_points[N] = {0, 1};
+
+/**
+ * @brief The components for the vertices in the z-axis for the donut's base
+ * sweep surface.
+ *
+ */
 static float z_points[N] = {0};
 
+/**
+ * @brief Draws the donut, and translates it to the location where `GL_LIGHT0`
+ * is being emitted from.
+ *
+ */
 void donut(void) {
-
   glPushMatrix();
+
   glColor3f(0.317f, 0.301f, 0.501f);
   glTranslatef(lx, ly, lz);
   glRotated(90.0, 0.0, 0.0, 1.0);
